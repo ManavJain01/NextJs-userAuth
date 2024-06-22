63c4a2834b11e913aa3edb98243201c380f7b595
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-06-22 21:31:33 +0530

    commit

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -23,10 +23,11 @@ export const sendEmail = async({email, emailType, userId}: any) => {
     })
 
     const mailOptions = {
-      from: 'manav@gmail.com',
+      from: 'rocker17manav@gmail.com',
       to: email,
       subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      html: `<p>Click <a href="${process.env.domain}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}</p>`
+      html: `<p>Click <a href="http://localhost:3000/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+      or copy and paste the link below in your browser. <br /> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`
 
     }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest){
   const path = request.nextUrl.pathname
 
-  const isPublicPath = path === '/login' || path === '/signup'
+  const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
 
   const token = request.cookies.get('token')?.value || ''
 
-  if(!isPublicPath && token){
+  if(isPublicPath && token){
     return NextResponse.redirect(new URL('/', request.nextUrl))
   }
 
@@ -19,5 +19,5 @@ export function middleware(request: NextRequest){
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/', '/profile','/login','/signup']
+  matcher: ['/', '/profile', '/login', '/signup', '/verifyemail']
 }
